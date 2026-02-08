b9144ca12abe7ebc8897aa3addec156b701c1854
ili <asghar.ali.z.a.r.e@gmail.com>
2026-02-08 16:07:24 +0330

    update

--- times/timezones.js
+++ times/timezones.js
@@ -17,11 +17,21 @@ const friendsTimezones = [
     {
         name: 'ili',
         timezone: 'GMT+3:30',
-        color: '#ff6f00'
+        color: '#ffffff'
     },
     {
         name: 'Sith',
         timezone: 'GMT-7',
         color: '#ff0606'
+    },
+    {
+        name: 'Komet',
+        timezone: 'GMT-5',
+        color: '#ff7b00'
+    },
+    {
+        name: 'JamJam',
+        timezone: 'GMT-8',
+        color: '#00ff00'
     }
 ];
