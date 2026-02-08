bef85b780b6cc7f3f1f00e52f1d0b07b96e8131a
ili <asghar.ali.z.a.r.e@gmail.com>
2026-02-08 13:05:49 +0330

    .

--- times/timezones.js
+++ times/timezones.js
@@ -1,22 +1,22 @@
 const friendsTimezones = [
     {
         name: 'Cash',
-        timezone: 'America/Denver', // Mountain Time (MDT is UTC-7)
+        timezone: 'GMT-7',
         color: '#30a15d'
     },
     {
         name: 'Aera',
-        timezone: 'Asia/Singapore', // Singapore Time (UTC+8, no DST)
+        timezone: 'GMT+8',
         color: '#ee93f1'
     },
     {
         name: 'Para',
-        timezone: 'America/Los_Angeles', // Pacific Time (PDT is UTC-7, PST is UTC-8)
+        timezone: 'GMT-8',
         color: '#2885b4'
     },
     {
         name: 'ili',
-        timezone: 'Asia/Tehran', // Iran Standard Time (UTC+3:30)
+        timezone: 'GMT+3:30',
         color: '#a3a3a3'
     }
 ];
