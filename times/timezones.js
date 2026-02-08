cc5036708eb659013c02e4189aa52b0f5bfc31cd
ili <asghar.ali.z.a.r.e@gmail.com>
2026-02-08 12:46:12 +0330

    first commit

---
+++ times/timezones.js
@@ -0,0 +1,17 @@
+const friendsTimezones = [
+    {
+        name: 'Sarah',
+        timezone: 'GMT-4', // New York (EDT)
+        color: '#ff6b6b'
+    },
+    {
+        name: 'Kenji',
+        timezone: 'GMT+9', // Tokyo
+        color: '#48dbfb'
+    },
+    {
+        name: 'Maria',
+        timezone: 'GMT+2', // Madrid (CEST)
+        color: '#1dd1a1'
+    }
+];
