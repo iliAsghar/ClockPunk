f761ea3d29be629a8e211847a2bae3be52c6b658
ili <asghar.ali.z.a.r.e@gmail.com>
2026-02-08 12:59:42 +0330

    daylight savings

--- times/timezones.js
+++ times/timezones.js
@@ -1,22 +1,22 @@
 const friendsTimezones = [
     {
         name: 'Cash',
-        timezone: 'GMT-7',
+        timezone: 'America/Denver', // Mountain Time (MDT is UTC-7)
         color: '#30a15d'
     },
     {
         name: 'Aera',
-        timezone: 'GMT+8',
+        timezone: 'Asia/Singapore', // Singapore Time (UTC+8, no DST)
         color: '#ee93f1'
     },
     {
         name: 'Para',
-        timezone: 'GMT-8',
+        timezone: 'America/Los_Angeles', // Pacific Time (PDT is UTC-7, PST is UTC-8)
         color: '#2885b4'
     },
     {
         name: 'ili',
-        timezone: 'GMT+3:30',
+        timezone: 'Asia/Tehran', // Iran Standard Time (UTC+3:30)
         color: '#a3a3a3'
     }
 ];
